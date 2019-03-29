3970ca15d0342bc8e3932c40f9bea15193882e29
I Nyoman Wiryanata <inyoman.wiryanata@gmail.com>
2019-03-29 14:59:22 +0700

    add visibility

--- corelibrary/src/main/java/com/inyomanw/corelibrary/utils/Ext.kt
+++ corelibrary/src/main/java/com/inyomanw/corelibrary/utils/Ext.kt
@@ -5,3 +5,11 @@ import android.view.View
 fun View.goneIf(boolean: Boolean) {
     if (boolean) this.visibility = View.GONE else this.visibility = View.VISIBLE
 }
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+fun View.visible(){
+    this.visibility = View.VISIBLE
+}
